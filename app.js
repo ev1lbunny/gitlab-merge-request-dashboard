441984f78b931e47cd2f42a78e9095d446ffed6e
Karl Malkin <kmalkin@williamhill.co.uk>
2019-03-19 13:51:52 +0000

    GIT-0001: updated code to use PUG for a views engine

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index')
 });
 
 const server = app.listen(7000, () => {
