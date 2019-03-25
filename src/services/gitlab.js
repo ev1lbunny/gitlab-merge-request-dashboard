cfd1e950417f44b5f5e3907ce1138a8f4640cb54
Karl Malkin <kmalkin@williamhill.co.uk>
2019-03-25 08:44:22 +0000

    GIT-0001: removed the gitlab dependency . in favour of building own restful service

--- src/services/gitlab.js
+++ src/services/gitlab.js
@@ -1,13 +1,3 @@
 /*eslint no-unused-vars: "warn" */
 
-const gitlab = require('gitlab/dist/es5').default
-
-const api = new gitlab({
-    url:   'https://git.nonprod.williamhill.plc',
-    token: 'z7zE4hsPaYUoo7VnkBVM',
-    rejectUnauthorized: false
-  })
-
-api.Projects.all()
-
 module.exports = gitlab
