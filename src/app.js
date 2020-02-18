9fdd2e46b1c641a777c16c93a750d6ba26e2255a
Billy Howard <billy@osg.me>
2020-02-18 16:12:02 +0000

    simplyfy the config code. removed the merge as I couldn't see a scenario where merging would be valid? happy to add back in.

--- src/app.js
+++ src/app.js
@@ -13,10 +13,9 @@ const path = require('path')
 /* Config Loading Requirements */
 const _ = require('lodash')
 const config = require('./config/app.json')
-const defaultConfig = config.dev
-const environment = process.env.NODE_ENV || 'DEV'
+const environment = process.env.NODE_ENV || 'dev'
 const environmentConfig = config[environment]
-const finalConfig = _.merge(defaultConfig, environmentConfig)
+const finalConfig = environmentConfig
 global.gConfig = finalConfig;
 
 /* Setup Morgen use tokens */
